417405607e4dd4220867793c53c4e92083d50a46
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-09-07 11:21:27 +0200

    Update HomePage.vue
    changes in navbar

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -4,26 +4,22 @@
         <div class="max-w-6xl mx-auto px-4">
             <nav class="pt-14">
                 <ul
-                    class="flex flex-col lg:flex-row border-b border-gray-200 lg:space-x-9 text-sm font-medium tracking-wider text-gray-400 ">
-                    <li class="hover:text-base border-b-2 border-blue-400 text-blue-400 pb-2 px-2">
-                        <a href="">FOOD</a>
-                        
-                    </li>
-                    <li class="hover:text-gray-700 pb-2 px-2">
-                        <a href="">NATURE</a>
-                    </li>
-                    <li class="hover:text-gray-700 pb-2 px-2">
-                        <a href="">TECHNOLOGY</a>
-                    </li>
-                    <li class="hover:text-gray-700 pb-2 px-2">
-                        <a href="">HEALTH</a>
+                    class="flex flex-col lg:flex-row border-b border-gray-200 lg:space-x-5 text-sm font-medium tracking-wider text-gray-400 ">
+
+                    <li v-for="category in categories"
+                    :key="category.id"
+                    :class="{'border-b-2 border-blue-400 text-blue-400': category.id == selectedCategoryId}"
+                    class="pb-2 px-2 text-gray-400">
+                        <a href="#" @click.prevent="categoryClicked(category)">
+                            {{ category.name }}
+                        </a>
                     </li>
                 </ul>
 
             </nav>
             <div class="pt-8 pl-0 grid grid-cols-2 lg:grid-cols-4 gap-6">
 
-                <Postitem v-for="item in items" :key="item.id" :post="item" />
+                <Postitem   v-for="item in items" :key="item.id" :post="item" />
 
             </div>
 
@@ -36,6 +32,7 @@
 import HomePageLayoutVue from "@/layouts/HomePageLayout.vue";
 import Postitem from "@/components/Posts/Postitem.vue";
 import PaginationVue from "@/components/Pagination/Pagination.vue";
+import axios from "axios";
 
 export default {
     name: ["HomePage"],
@@ -46,32 +43,60 @@ export default {
     },
     data() {
         return {
-            items: []
+            items: [],
+            categories:[
+                {id: null, name : "All"},
+            ],
+            selectedCategoryId:null,
+            currentPage: 1,
         }
     },
     mounted() {
-            this.getItems();
+            this.mountCategories();
+            this.mountItems();
       },
       methods: {
-            async getItems() {
-                  const items = await fetch('https://dummyjson.com/products?limit=8')
+        
+
+   formatDate(dateString) {
+            const date = new Date(dateString);
+            return new Intl.DateTimeFormat('default', {dateStyle: 'long'}).format(date);
+        },
+        async mountItems() {
+            const urlQueryParam = this.selectedCategoryId ? `category[]=${this.selectedCategoryId}`: null;
+                const items = await fetch(`https://ma.tenton.al/api/v1/posts?${urlQueryParam}`)
                         .then(res => res.json())
                         .then(res => {
                               return res;
                         });
 
-                  this.items = items.products.map((item) => {
-                        return {
-                              id: item.id,
-                              title: item.title,
-                              created_at: "Nov, 25, 2021",
-                              description: item.description,
-                              image: item.thumbnail,
-                              user: require(`@/assets/images/profileicon.svg`),
-                              comments_count: item.stock,
-                        }
+                this.items = items.data.map((item) => {
+                    return {
+                      id: item.id,
+                      title: item.title,
+                      created_at: this.formatDate(item.created_at),
+                      description: item.content,
+                      image: item.resources.length > 0 ? item.resources[0].preview_url : null,
+                      user: require(`@/assets/images/profileicon.svg`),
+                      comments_count: item.stock,
+                    }
                   })
-            }
+            },
+        async mountCategories(){
+            axios.get('https://ma.tenton.al/api/v1/base/post_categories')
+                    .then(res => {
+                          this.categories = res.data.data.slice(0, 4);
+
+                          this.categories.unshift({
+                            id: null, name: 'All'
+                          })
+                    })
+            },
+        categoryClicked(category){
+            this.selectedCategoryId = category.id;
+            this.mountItems();
+            },
+       
 }
 }
 
