c66bdd45a76a8e0b096aeb3e687dbda2d2b4a276
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-09-26 16:31:22 +0200

    Update Categories.vue

--- src/Pages/Profile/Categories/Categories.vue
+++ src/Pages/Profile/Categories/Categories.vue
@@ -14,7 +14,7 @@
             <Base-spinner v-if="isLoading"></Base-spinner>
       </div>
 
-      <div class="pt-8 pl-0 grid grid-cols-2 lg:grid-cols-4 gap-4">
+      <div class="pt-8 pl-0 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             <Postitem v-for="item in items" :key="item.id" :post="item" />
       </div>
 
