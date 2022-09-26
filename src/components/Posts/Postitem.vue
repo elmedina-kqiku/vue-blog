fdad80d17867d52728ff1102c6075ab35448392b
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-09-26 16:33:02 +0200

    Update Postitem.vue

--- src/components/Posts/Postitem.vue
+++ src/components/Posts/Postitem.vue
@@ -11,7 +11,7 @@
                 <img :src="post.resources[0]?.preview_url" alt="image" class=" rounded-lg w-full h-40 object-cover">
             </template>
             <template v-else>
-                <div class="w-60 h-40  ">
+                <div class="w-full h-40  ">
                     <img class="object-cover w-full rounded-lg h-full" src="@/assets/images/defaultImage.svg" alt=""/>
                 </div>
             </template>
