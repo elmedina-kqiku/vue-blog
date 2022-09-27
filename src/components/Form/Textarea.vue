495703bc03896338c665063373e26e574aacb271
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-09-27 13:52:56 +0200

    Create Textarea.vue

---
+++ src/components/Form/Textarea.vue
@@ -0,0 +1,20 @@
+<template>
+    <textarea 
+    :value="modelValue" @input="updateValue"
+    class="border border-gray-300 rounded-lg w-full px-3 py-2 text-xs text-gray-500"></textarea>
+</template>
+<script>
+export default {
+    name: ["Input"],
+    props: {
+        modelValue:{
+            default:"",
+        }
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
+}
+</script>
